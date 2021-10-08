b1bea58cd4f4afc05663b3f4a6f5c706f544a46e
Jorgen Arnes <jorgen.arnes@gmail.com>
2021-10-08 22:32:52 +0200

    uninstalled unused packages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
 import Layout from '../components/Layout'
 import TodoList from '../components/TodoList'
-
 
 export default function Home() {
   return (
